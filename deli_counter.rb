0ccbc5e12a716921e57a5a281505f23543346d49
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-02 15:31:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,5 +7,6 @@ def line(array)
   else
     return_string = "The line is currently: "
     return_string.push(array.each.with_index {|person,index| "#{index+1}. #{person} "})
+    puts return_string
   end
 end
