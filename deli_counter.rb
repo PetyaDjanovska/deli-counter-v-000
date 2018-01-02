9e0ec8ee43f236c48830960b177cb3c794d46cf5
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-02 15:29:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(array)
     "The line is currently empty."
   else
     return_string = "The line is currently: "
-    return_string.add(array.each.with_index {|person,index| "#{index+1}. #{person} "})
+    return_string.push(array.each.with_index {|person,index| "#{index+1}. #{person} "})
   end
 end
