9a3406c6b2ad7f6e39663aa5f08ea8a60c129781
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-02 15:30:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(array)
   if array.empty?
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     return_string = "The line is currently: "
     return_string.push(array.each.with_index {|person,index| "#{index+1}. #{person} "})
